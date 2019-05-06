763ff1cbf012a67d480d37d60737f6d9a4fb9e50
Werner Wank <14849239+wernerwa@users.noreply.github.com>
2019-05-06 20:07:30 +0200

    remove trailing whitespaces

--- I18Nv2/Locale/Windows.php
+++ I18Nv2/Locale/Windows.php
@@ -10,7 +10,7 @@ $windows = array(
     'de_DE' => 'deu',
     'de_AT' => 'dea',
     'de_CH' => 'des',
-    
+
     'en_US' => 'enu',
     'en_GB' => 'eng',
     'en_AU' => 'ena',
@@ -21,17 +21,17 @@ $windows = array(
     'en_PH' => 'enp',
     'en_ZA' => 'ens',
     'en_NZ' => 'enz',
-    
+
     'fr_FR' => 'fra',
     'fr_CH' => 'frs',
     'fr_BE' => 'frb',
     'fr_CA' => 'frc',
     'fr_LU' => 'frl',
     'fr_MC' => 'frm',
-    
+
     'it_IT' => 'ita',
     'it_CH' => 'its',
-    
+
     'es_ES' => 'esp',
     'es_PA' => 'esa',
     'es_BO' => 'esb',
@@ -49,38 +49,38 @@ $windows = array(
     'es_VE' => 'esv',
     'es_UY' => 'esy',
     'es_PY' => 'esz',
-    
+
 
     'pt_PT' => 'ptg',
     'pt_BR' => 'ptb',
-    
+
     'sv_SE' => 'sve',
     'sv_FI' => 'svf',
-    
+
     'no_NO' => 'nor',
     'nb_NO' => 'nor',
     'nn_NO' => 'non',
-    
+
     'fi_FI' => 'fin',
-    
+
     'is_IS' => 'isl',
-    
+
     'da_DK' => 'dan',
-    
+
     'nl_NL' => 'nld',
     'nl_BE' => 'nlb',
-    
+
     'ru_RU' => 'rus',
-    
+
     'hu_HU' => 'hun',
-    
+
     'ru_RU' => 'rus',
-    
+
     'af_ZA' => 'afk',
-    
+
     'sl_SI' => 'slv',
-    
+
     'pl_PL' => 'plk',
-    
+
     'cs_CZ' => 'csy',
 );
